c2c68a178550c51b8a05dffa5e0ba49ce797599a
AOx0 <50227494+AOx0@users.noreply.github.com>
2022-03-19 02:42:25 -0600

    Init

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const path = require("path");
+
+const app = express()
+const port = 80
+
+let USERS = []
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo")
+})
+
+app.get("/auth/register/:nombre/:correo/:apellido/:contrasenna", (req, res) => {
+
+  exito = true
+
+  for (i=0; i<USERS.length; i++) {
+    if (req.params.correo == USERS[i].correo) {
+      res
+        .send("Registro fallido")
+        .status(400)
+
+      exito = false
+      return
+    }
+  }
+
+  if (exito) {
+    USERS.push({
+      nombre: req.params.nombre,
+      correo: req.params.correo,
+      apellido: req.params.apellido,
+      contrasenna: req.params.contrasenna
+    })
+
+
+
+    res
+      .send("Registro exitoso")
+      .status(200)
+  }
+
+  console.log(USERS)
+})
+
+app.listen(port, () => {
+  console.log("Escuchando en ", port)
+})
